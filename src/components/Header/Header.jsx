5fe629dcc3a5c1756271c9199ce52dccc9cddcb2
Douglas Rosenberg <doug.rosenberg@gmail.com>
2021-10-04 13:26:12 -0500

    2323

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,31 @@
 import React from 'react';
+import { Autocomplete } from '@material-ui/lab';
+import { AppBar, Toolbar, Typography, InputBase, Box } from '@material-ui/core';
+import { mergeClasses } from '@material-ui/styles';
+import SearchIcon from '@material-ui/icons';
 
 const Header = () => {
     return (
-        <h1>Header</h1>
+        <AppBar position='static'>
+            <Toolbar className={mergeClasses.toolBar}>
+                <Typography variant='h5' className={classes.title}>
+                    Travel Advisor
+                </Typography>
+                <Box>
+                    <Typography variant='h6' className={classes.title}>
+                        Explore new places
+                    </Typography>  
+                    <Autocomplete>
+                        <div className={classes.search}>
+                            <div className={classes.searchIcon}>
+                                <SearchIcon />
+                            </div>
+                            <InputBase />
+                        </div>
+                    </Autocomplete>
+                </Box>
+            </Toolbar>
+        </AppBar>
     );
 }
 
