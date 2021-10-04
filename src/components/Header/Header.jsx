135c0e589320f86bba49f565e1d0d3dc1a35b39e
Douglas Rosenberg <doug.rosenberg@gmail.com>
2021-10-04 14:24:08 -0500

    2740

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,28 +1,30 @@
 import React from 'react';
 import { Autocomplete } from '@material-ui/lab';
 import { AppBar, Toolbar, Typography, InputBase, Box } from '@material-ui/core';
-import { mergeClasses } from '@material-ui/styles';
-import SearchIcon from '@material-ui/icons';
+import SearchIcon from '@material-ui/icons/Search';
+
+import useStyles from './styles';
 
 const Header = () => {
+    const classes = useStyles();
     return (
         <AppBar position='static'>
-            <Toolbar className={mergeClasses.toolBar}>
+            <Toolbar className={classes.toolBar}>
                 <Typography variant='h5' className={classes.title}>
                     Travel Advisor
                 </Typography>
-                <Box>
+                <Box display='flex'>
                     <Typography variant='h6' className={classes.title}>
                         Explore new places
                     </Typography>  
-                    <Autocomplete>
+                    {/* <Autocomplete> */}
                         <div className={classes.search}>
                             <div className={classes.searchIcon}>
                                 <SearchIcon />
                             </div>
-                            <InputBase />
+                            <InputBase placeholder='Search...' classes={{ root: classes.inputRoot, input: classes.inputInput }} />
                         </div>
-                    </Autocomplete>
+                    {/* </Autocomplete> */}
                 </Box>
             </Toolbar>
         </AppBar>
